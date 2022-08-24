e80ab42fa0d342cf3b029b84beb9328da021f0e6
Timur <timurka.zavyalov@mail.ru>
2022-08-24 15:30:51 +0300

    fix: changed columns in reference_tnved

--- scripts_for_bash_with_inheritance/reference_tnved.py
+++ scripts_for_bash_with_inheritance/reference_tnved.py
@@ -1,56 +1,39 @@
+import contextlib
+import datetime
 import json
-import math
 import os
 import sys
-import contextlib
-import numpy as np
 import pandas as pd
+
+
+def default(o):
+    if isinstance(o, (datetime.date, datetime.datetime)):
+        return o.isoformat()
+
 
 input_file_path = os.path.abspath(sys.argv[1])
 output_folder = sys.argv[2]
 
-headers_eng = {
-    "Год": "year",
-    "Мес": "month",
-    "Отгружен": "shipment_date",
-    "Терминал": "terminal",
-    "Направление": "direction",
-    "Линия": "line",
-    "Экспедитор": "shipping_agent",
-    "Отправитель (исходное название)": "shipper_name",
-    "Номер контейнера": "container_number",
-    "Порт (предобработка)": "destination_port",
-    "Страна (предобратока)": "destination_country",
-    "Груз": "goods_name",
-    "TEU": "teu",
-    "Размер контейнера": "container_size",
-    "Тип контейнера": "container_type",
-    "Группа груза по ТНВЭД (проставляется вручную через код ТНВЭД - ячека Х)": "tnved_group_id",
-    "Наименование Группы (подтягивается по коду через справочник)": "tnved_group_name",
-    "ИНН (извлечен через excel)": "shipper_inn",
-    "УНИ-компания (подтянута через ИНН)": "shipper_name_unified",
-    "Страна": "shipper_country",
-    "Регион компании": "shipper_region",
-    "Номер ГТД": "gtd_number",
-    "Тип Парка": "park_type",
-    "ТНВЭД": "tnved",
-    "Судно": "ship_name",
-    "Получатель": "consignee_name",
-    "Букинг": "booking"
-}
+headers_eng = ["section_tnved", "group_tnved", "goods_name", "notation", "start_date_group", "expire_date_group"]
 
 
+# df = pd.read_csv(input_file_path, names=headers_eng, dtype=str)
 df = pd.read_csv(input_file_path, dtype=str)
-df = df.replace({np.nan: None})
-df = df.rename(columns=headers_eng)
-df = df.loc[:, df.columns.isin(['tnved', 'tnved_group_id', 'tnved_group_name'])]
+df.columns = headers_eng
+# df = df.loc[:, ~df.columns.isin(['unnamed'])]
+df[df.columns] = df.apply(lambda x: x.str.strip())
+df["start_date_group"] = pd.to_datetime(df["start_date_group"], dayfirst=True).dt.date
+df["expire_date_group"] = pd.to_datetime(df["expire_date_group"], dayfirst=True).dt.date
+df.replace({pd.NaT: None}, inplace=True)
 parsed_data = df.to_dict('records')
+
 for dict_data in parsed_data:
     for key, value in dict_data.items():
         with contextlib.suppress(Exception):
-            if key == 'tnved_group_id':
+            if key in ['section_tnved', 'group_tnved']:
                 dict_data[key] = f"{int(value):02d}"
+
 basename = os.path.basename(input_file_path)
 output_file_path = os.path.join(output_folder, f'{basename}.json')
 with open(f"{output_file_path}", 'w', encoding='utf-8') as f:
-    json.dump(parsed_data, f, ensure_ascii=False, indent=4)
+    json.dump(parsed_data, f, ensure_ascii=False, indent=4, default=default)
