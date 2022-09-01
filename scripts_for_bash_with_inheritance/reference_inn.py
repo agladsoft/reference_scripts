ead704e85042c8dea6ce477a9a61bd4b26e596e2
Timur <timurka.zavyalov@mail.ru>
2022-09-01 14:05:22 +0300

    fix: changed columns names and remove spaces (strip)

--- scripts_for_bash_with_inheritance/reference_inn.py
+++ scripts_for_bash_with_inheritance/reference_inn.py
@@ -7,9 +7,25 @@ import pandas as pd
 input_file_path = os.path.abspath(sys.argv[1])
 output_folder = sys.argv[2]
 
+headers_eng = {
+    "Компания": "company_name",
+    "ИНН": "company_inn",
+    "УНИ-компания": "company_name_unified"
+}
+
+
+def trim_all_columns(df):
+    """
+    Trim whitespace from ends of each value across all series in dataframe
+    """
+    trim_strings = lambda x: x.strip() if isinstance(x, str) else x
+    return df.applymap(trim_strings)
+
 
 df = pd.read_csv(input_file_path, dtype=str)
 df = df.replace({np.nan: None})
+df = df.rename(columns=headers_eng)
+df = trim_all_columns(df)
 parsed_data = df.to_dict('records')
 basename = os.path.basename(input_file_path)
 output_file_path = os.path.join(output_folder, f'{basename}.json')
