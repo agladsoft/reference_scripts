6ed1ad8079d8039276034199873e2d8377a30dad
timur <timurka.zavyalov@mail.ru>
2023-03-03 12:29:30 +0500

    add: added rzhd

--- bash_dir/_all_references.sh
+++ bash_dir/_all_references.sh
@@ -8,4 +8,4 @@ ${XL_IDP_PATH_REFERENCE_SCRIPTS}/bash_dir/reference_ship.sh
 ${XL_IDP_PATH_REFERENCE_SCRIPTS}/bash_dir/reference_is_empty.sh
 ${XL_IDP_PATH_REFERENCE_SCRIPTS}/bash_dir/reference_inn.sh
 ${XL_IDP_PATH_REFERENCE_SCRIPTS}/bash_dir/reference_tnved2.sh
-#${XL_IDP_PATH_REFERENCE_SCRIPTS}/bash_dir/reference_inn_by_api.sh
+#${XL_IDP_PATH_REFERENCE_SCRIPTS}/bash/reference_inn_by_api.sh
