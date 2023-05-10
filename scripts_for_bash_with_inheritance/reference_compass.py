e9f3306ebc1260aa341a70ae631d975810212934
timur <timurka.zavyalov@mail.ru>
2023-05-10 15:23:16 +0300

    add: added func leave_largest_data_with_dupl_inn (refactoring)

--- scripts_for_bash_with_inheritance/reference_compass.py
+++ scripts_for_bash_with_inheritance/reference_compass.py
@@ -68,7 +68,7 @@ class ReferenceCompass(object):
         client: Client = get_client(host=os.getenv('HOST'), database=os.getenv('DATABASE'),
                                     username=os.getenv('USERNAME_DB'), password=os.getenv('PASSWORD'))
         client.query("SET allow_experimental_lightweight_delete=1")
-        parsed_data_copy = parsed_data.copy()
+        parsed_data_copy: list = parsed_data.copy()
         for dict_data in parsed_data_copy:
             for key, value in dict_data.items():
                 if key in ["inn"]:
