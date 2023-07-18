76e0d287cdd2d55b9dd212fd07e90238a6fd0afb
timur <timurka.zavyalov@mail.ru>
2023-07-18 17:15:05 +0300

    fix: fixed calculate teu in morservice

--- scripts_for_bash_with_inheritance/reference_morservice_all.py
+++ scripts_for_bash_with_inheritance/reference_morservice_all.py
@@ -108,7 +108,8 @@ class ReferenceMorService(object):
                     "is_empty": current_line[1] == 'порожние',
                     "container_type": 'REF' if current_line[1] == 'из них реф.' else None,
                     "teu": self.parse_float(current_line[indexes[1]]) - self.parse_float(next_line[indexes[1]])
-                    if current_line[1] == 'груженые' and current_line[indexes[1]] and next_line[indexes[1]]
+                    if current_line[1] == 'груженые' and current_line[indexes[1]].isdigit()
+                       and next_line[indexes[1]].isdigit()
                     else self.parse_float(current_line[indexes[1]])
                 }
                 parsed_data.append(self.merge_two_dicts(context, parsed_record))
