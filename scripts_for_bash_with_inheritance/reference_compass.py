87a13f3e0cabc78e22c3a50fd457f35a8bbf3de3
timur <timurka.zavyalov@mail.ru>
2023-08-24 16:05:02 +0300

    fix: not count liquidated companies

--- scripts_for_bash_with_inheritance/reference_compass.py
+++ scripts_for_bash_with_inheritance/reference_compass.py
@@ -273,7 +273,7 @@ class ReferenceCompass(object):
                 company_address: dict = company_data.get("address")
                 company_address_data: dict = company_address.get("data", {})
                 company_data_branch: dict = company_data.get("branch_type")
-                if company_data and company_address:
+                if company_data and company_data["state"]["status"] != "LIQUIDATED":
                     self.add_dadata_columns(company_data, company_address, company_address_data, company_data_branch,
                                             company, dict_data)
                     self.cache_add_and_save(dict_data)
