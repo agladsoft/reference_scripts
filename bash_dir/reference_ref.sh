1681117ad1a73119ce4a4ff43a447900477b3a3a
timur <timurka.zavyalov@mail.ru>
2023-10-10 15:34:34 +0300

    add: added reference_ref

--- bash_dir/reference_ref.sh
+++ bash_dir/reference_ref.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-xls_path="${XL_IDP_PATH_REFERENCE}/reference_ref/"
-
-csv_path="${xls_path}"/csv
-if [ ! -d "$csv_path" ]; then
-  mkdir "${csv_path}"
-fi
+xls_path="${XL_IDP_PATH_REFERENCE}/reference_ref"
 
 done_path="${xls_path}"/done
 if [ ! -d "$done_path" ]; then
@@ -17,8 +12,9 @@ if [ ! -d "$json_path" ]; then
   mkdir "${json_path}"
 fi
 
-find "${xls_path}" -maxdepth 1 -type f \( -name "*.xls*" -or -name "*.XLS*" \) ! -newermt '3 seconds ago' -print0 | while read -d $'\0' file
+find "${xls_path}" -maxdepth 1 -type f \( -name "*.xls*" -or -name "*.XLS*" -or -name "*.xml" \) ! -newermt '3 seconds ago' -print0 | while read -d $'\0' file
 do
+
   if [[ "${file}" == *"error_"* ]];
   then
     continue
@@ -27,38 +23,14 @@ do
 	mime_type=$(file -b --mime-type "$file")
   echo "'${file} - ${mime_type}'"
 
-  csv_name="${csv_path}/$(basename "${file}").csv"
-
-  if [[ ${mime_type} = "application/vnd.ms-excel" ]]
-  then
-    echo "Will convert XLS '${file}' to CSV '${csv_name}'"
-    in2csv -f xls "${file}" > "${csv_name}"
-  elif [[ ${mime_type} = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ]]
-  then
-    echo "Will convert XLSX or XLSM '${file}' to CSV '${csv_name}'"
-    in2csv -f xlsx "${file}" > "${csv_name}"
-  else
-    echo "ERROR: unsupported format ${mime_type}"
-    mv "${file}" "${xls_path}/error_$(basename "${file}")"
-    continue
-  fi
+	# Will convert csv to json
+	python3 ${XL_IDP_PATH_REFERENCE_SCRIPTS}/scripts_for_bash_with_inheritance/reference_ref.py "${file}" "${json_path}"
 
   if [ $? -eq 0 ]
 	then
 	  mv "${file}" "${done_path}"
 	else
 	  mv "${file}" "${xls_path}/error_$(basename "${file}")"
-	  echo "ERROR during convertion ${file} to csv!"
-	  continue
 	fi
 
-	# Will convert csv to json
-	python3 ${XL_IDP_PATH_REFERENCE_SCRIPTS}/scripts_for_bash_with_inheritance/reference_ref.py "${csv_name}" "${xls_path}"/json/$(basename "${csv_name}")
-
-  if [ $? -eq 0 ]
-	then
-	  mv "${csv_name}" "${done_path}"
-	else
-	  mv "${csv_name}" "${xls_path}/error_$(basename "${csv_name}")"
-	fi
 done
