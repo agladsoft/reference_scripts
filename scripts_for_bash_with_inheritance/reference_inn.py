4665be946797b479b50e6e2f517ddea80f5a68ff
timur <timurka.zavyalov@mail.ru>
2023-11-28 15:21:23 +0300

    feat: added dadata cache

--- scripts_for_bash_with_inheritance/reference_inn.py
+++ scripts_for_bash_with_inheritance/reference_inn.py
@@ -8,6 +8,7 @@ import re
 import sys
 import numpy as np
 import pandas as pd
+import csv
 from deep_translator import GoogleTranslator
 from fuzzywuzzy import fuzz
 from dadata import Dadata
@@ -46,11 +47,7 @@ def trim_all_columns(df):
     return df.applymap(trim_strings)
 
 
-df = pd.read_csv(input_file_path, dtype=str)
-df['company_name_rus'] = None
-df['is_inn_found_auto'] = False
-df['confidence_rate'] = None
-df['company_name_dadata_unified'] = None
+df = pd.read_excel(input_file_path, dtype=str)
 df = df.replace({np.nan: None})
 df = df.dropna(axis=0, how='all')
 df = df.rename(columns=headers_eng)
@@ -70,37 +67,32 @@ def get_company_name_from_dadata(value: str, dadata_name: str = None):
         return dadata_name
 
 
-def parse_data(i, dict_data):
+def parse_data(i, dict_data, parsed_data):
     for key, value in dict_data.items():
         with contextlib.suppress(Exception):
             if key == 'company_inn':
-                dict_data["company_name_dadata_unified"] = get_company_name_from_dadata(value)
-            elif key == 'company_name':
-                company_name_rus = GoogleTranslator(source='en', target='ru').translate(value)
-                dict_data['company_name_rus'] = company_name_rus
-            elif key == 'company_name_unified':
-                company_name_unified = value
-            elif key == 'confidence_rate':
-                company_name_unified = re.sub(" +", " ", company_name_unified)
-                company_name_rus = re.sub(" +", " ", company_name_rus)
-                company_name_unified = company_name_unified.translate({ord(c): " " for c in ",'!@#$%^&*()[]{};<>?\|`~=_+"})
-                company_name_rus = company_name_rus.translate({ord(c): "" for c in ",'!@#$%^&*()[]{};<>?\|`~=_+"})
-                dict_data['confidence_rate'] = fuzz.partial_ratio(company_name_unified.upper(), company_name_rus.upper())
-    dict_data['original_file_name'] = os.path.basename(input_file_path)
-    dict_data['original_file_parsed_on'] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                dict_data["company_name_unified"] = get_company_name_from_dadata(value)
+    return dict_data
 
-    logging.info(f'{i} data is {dict_data}')
-    logger_stream.info(f'{i} data is {dict_data}')
-    basename = os.path.basename(input_file_path)
-    output_file_path = os.path.join(output_folder, f'{basename}_{i}.json')
-    with open(f"{output_file_path}", 'w', encoding='utf-8') as f:
-        json.dump(dict_data, f, ensure_ascii=False, indent=4)
-
+    # logging.info(f'{i} data is {dict_data}')
+    # logger_stream.info(f'{i} data is {dict_data}')
+    # basename = os.path.basename(input_file_path)
+    # output_file_path = os.path.join(output_folder, f'{basename}_{i}.json')
+    # with open(f"{output_file_path}", 'w', encoding='utf-8') as f:
+    #     json.dump(dict_data, f, ensure_ascii=False, indent=4)
 
 procs = []
+list_all_data = []
 with Pool(processes=worker_count) as pool:
     for i, dict_data in enumerate(parsed_data, 2):
-        proc = pool.apply_async(parse_data, (i, dict_data))
-        procs.append(proc)
+        proc = pool.apply_async(parse_data, (i, dict_data, parsed_data), callback=list_all_data.append)
+    pool.close()
+    pool.join()
+    print(list_all_data)
 
-    results = [proc.get() for proc in procs]
+basename = os.path.basename(input_file_path)
+output_file_path = os.path.join(output_folder, f'{basename}.csv')
+with open(f"{output_file_path}", 'w', encoding='utf-8') as f:
+    fc = csv.DictWriter(f, fieldnames=list_all_data[0].keys())
+    fc.writeheader()
+    fc.writerows(list_all_data)
