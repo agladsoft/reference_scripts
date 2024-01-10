6912232697ea3afa62630a9d710cee5b2cceb779
uvens <uventus1992@gmail.com>
2024-01-10 16:09:33 +0300

    add telegram notification

--- scripts_for_bash_with_inheritance/__init__.py
+++ scripts_for_bash_with_inheritance/__init__.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from notifiers import get_notifier
 
 LIST_MONTHS: list = [
@@ -16,6 +17,7 @@ LIST_MONTHS: list = [
     "декабрь"
 ]
 
+
 def get_my_env_var(var_name: str) -> str:
     try:
         return os.environ[var_name]
@@ -26,6 +28,16 @@ def get_my_env_var(var_name: str) -> str:
 class MissingEnvironmentVariable(Exception):
     pass
 
+
 def telegram(message):
-    teg = get_notifier('telegram')
-    teg.notify(token=get_my_env_var('TOKEN'), chat_id=get_my_env_var('CHAT_ID'), message=message)
+    # teg = get_notifier('telegram')
+    # teg.notify(token=get_my_env_var('TOKEN'), chat_id=get_my_env_var('CHAT_ID'), message=message)
+    chat_id = get_my_env_var('CHAT_ID')
+    token = get_my_env_var('TOKEN')
+    topic = get_my_env_var('TOPIC')
+    message_id = get_my_env_var('ID')
+    # teg.notify(token=get_my_env_var('TOKEN'), chat_id=get_my_env_var('CHAT_ID'), message=message)
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {"chat_id": f"{chat_id}/{topic}", "text": message,
+              'reply_to_message_id': message_id}  # Добавляем /2 для указания второго подканала
+    response = requests.get(url, params=params)
