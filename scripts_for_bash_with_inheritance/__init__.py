e31ba9c2465d0f385a468c4751fe85dca0341f59
uventus <uventus_work@mail.ru>
2025-08-26 12:37:39 +0300

    feat: реализована дополнительная отправка уведомлений по SMTP на email - Добавлена SMTP-интеграция для отправки писем - Настроена отправка уведомлений на указанные адреса

--- scripts_for_bash_with_inheritance/__init__.py
+++ scripts_for_bash_with_inheritance/__init__.py
@@ -29,6 +29,27 @@ def get_my_env_var(var_name: str) -> str:
 class MissingEnvironmentVariable(Exception):
     pass
 
+def send_email_notifiers(message: str, subject: str = "Уведомление от системы экспорта"):
+    """
+    Отправка email через Mail.ru
+    """
+    try:
+        email = get_notifier('email')
+        email.notify(
+            to=get_my_env_var('RECIPIENT_EMAIL'),
+            subject=subject,
+            message=message,
+            from_=get_my_env_var('EMAIL_USER'),
+            host='smtp.mail.ru',
+            port=587,
+            username=get_my_env_var('EMAIL_USER'),
+            password=get_my_env_var('EMAIL_PASSWORD'),
+            tls=True
+        )
+        print(f"Email успешно отправлен на {get_my_env_var('RECIPIENT_EMAIL')}")
+    except Exception as e:
+        print(f"Ошибка при отправке email: {e}")
+
 
 def telegram(message):
     # teg = get_notifier('telegram')
@@ -40,5 +61,6 @@ def telegram(message):
     # teg.notify(token=get_my_env_var('TOKEN'), chat_id=get_my_env_var('CHAT_ID'), message=message)
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     params = {"chat_id": f"{chat_id}/{topic}", "text": message,
-              'reply_to_message_id': message_id}  # Добавляем /2 для указания второго подканала
+              'reply_to_message_id': message_id}
+    send_email_notifiers(params.get('text'))# Добавляем /2 для указания второго подканала
     response = requests.get(url, params=params)
